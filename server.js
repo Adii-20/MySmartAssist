c25c642685b4f58a2e47a37d55fc67b593156195
Adii-20 <shelaraditya721@gmail.com>
2025-10-03 11:40:25 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const webhook = require('./n8n/webhook');
+const webhook = require("./webhook");
 
 
 app.use(express.json());
